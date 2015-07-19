f6911454dc5570af4bea668483167da8ec877f0c
ChihAn Lai <chihan.lai@gmail.com>
2015-07-19 21:46:01 +0800

    finish ex5, but there are some differneces between my result and ex5.pdf, and I believe I am right haha

--- ex5/linearRegBiasVariance.py
+++ ex5/linearRegBiasVariance.py
@@ -18,38 +18,47 @@ def  linearRegCostFunction(x, y, theta, lamda):
     m, n = x.shape
     if(theta.ndim == 1):
         theta = theta[:, None]
-    tmp = np.dot(x, theta)-y
+    tmp = np.dot(x, np.copy(theta))-y
     theta[0,0] = 0
-    cost = np.sum(np.multiply(tmp, tmp))/(2*m) + np.sum(np.multiply(theta, theta))*lamda/(2*m)
+    cost = np.sum(np.multiply(tmp, tmp))/(2*m) + (np.sum(np.multiply(np.copy(theta), np.copy(theta)))*lamda/(2*m))
     grad = ((np.dot(np.transpose(x), tmp))/(m))+(theta*(lamda)/(m))
-    # grad.flatten makes grad to be a 1-D array, and it can help when used in fmin_cg
+    # grad.flatten makes grad to be a 1-D array, 
+    # and it can help when used in fmin_cg
     return cost, grad.flatten()
 
 def trainLinearReg(x, y, lamda):
     m, n = x.shape
     theta = np.zeros((n, 1))
-    costFunction = lambda  theta: linearRegCostFunction(x, y, np.copy(theta), lamda)[0] # AGAIN, np.copy(theta), np.copy here is fucking important, pass by value not by object.
-    costGrad = lambda theta: linearRegCostFunction(x, y, np.copy(theta), lamda)[1]
+    # AGAIN, np.copy(theta), np.copy here is fucking important,
+    # pass by value not by object. 
+    costFunction = lambda  theta: linearRegCostFunction(x, y, \
+                                                        (theta), \
+                                                        lamda)[0] 
+    costGrad = lambda theta: linearRegCostFunction(x, y, \
+                                                    (theta), \
+                                                    lamda)[1]
     #when using fmin_cg, the initial value must be 1-D array
-    xopt = fmin_cg(costFunction, np.copy(theta), fprime=costGrad, gtol=1e-09, maxiter=200)
+    xopt = fmin_cg(costFunction, np.copy(theta), \
+                    fprime=costGrad, gtol=1e-09, maxiter=200)
+    
     return xopt
 
+
 def learningCurve(x, y, xv, yv, lamda):
-    #print(x, y, xv, yv)
     m, n = x.shape
     errTrain = np.zeros((m, 1))
     errVal = np.zeros((m, 1))
     for i in range(m):
         theta = trainLinearReg(x[0:i+1,:],y[0:i+1,:], lamda)
         theta = theta[:,None]
-        errTrain[i] = linearRegCostFunction(x[0:i+1,:], y[0:i+1,:], 
+        errTrain[i] = linearRegCostFunction(x[0:i+1,:], y[0:i+1,:], \
                                             np.copy(theta), lamda)[0]
         errVal[i] = linearRegCostFunction(xv, yv, np.copy(theta), lamda)[0]
     return errTrain, errVal
 
 def polyFeatures(x, p):
     m, n = x.shape
-    polyX = np.zeros(shape=(m, p))
+    polyX = np.zeros(shape=(m, p)) # edit here
     for i in range(p):
         polyX[:, i] = x[:, 0]**(i+1)
     return polyX
@@ -57,8 +66,26 @@ def polyFeatures(x, p):
 def featureNormalize(x):
     mean = np.mean(x, axis=0)
     tmp = (x - mean)
-    std = np.std(tmp, axis=0)
+    # ddof is Delta Degrees of Freedom,
+    # the divisor used in calculations is N - ddof
+    std = np.std(tmp, axis=0, ddof=0) 
     normX = tmp/std
     return normX, mean, std
 
+def validationCurve(polyX, y, polyXv, yv):
+    lamdaVec = [0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10]
+    errTrain = np.zeros((len(lamdaVec)))
+    errVal = np.zeros((len(lamdaVec)))
+    for i in range(len(lamdaVec)):
+        theta = trainLinearReg(polyX, y, lamdaVec[i])
+        errTrain[i] = linearRegCostFunction(polyX, y, np.copy(theta), lamdaVec[i])[0]
+        errVal[i] = linearRegCostFunction(polyXv, yv, np.copy(theta), lamdaVec[i])[0]
+    return lamdaVec, errTrain, errVal
 
+def plotFit(xmin, xmax, mean, std, theta, p):
+    x = np.arange(xmin-15, xmax+25, 0.05)
+    x = x[:,None]
+    polyX = polyFeatures(np.copy(x), p)
+    polyX = (polyX - mean)/std
+    polyX = np.concatenate((np.ones((polyX.shape[0], 1)), polyX), axis=1)
+    plt.plot(x, np.dot(polyX, theta))
--- ex5/test/ex5.py
+++ ex5/test/ex5.py
@@ -8,7 +8,7 @@ from scipy.optimize import fmin_cg
 import numpy as np
 import matplotlib.pyplot as plt
 
-#change python current working directory to the current folder
+# change python current working directory to the current folder
 os.chdir(os.path.dirname(sys.argv[0]))
 #print(os.getcwd())
 
@@ -25,41 +25,105 @@ yt = data['ytest']
 print('x shape =', x.shape, '\ny shape =', y.shape)
 print('xv shape =', xv.shape, '\nyv shape =', yv.shape)
 print('xt shape =', xt.shape, '\nyt shape =', yt.shape)
-'''
+
 plt.plot(x, y, 'ro')
 plt.xlabel('Change in water level (x)')
 plt.ylabel('Water flowing out of the dam (y)')
-plt.show()
+
 input('Program paused. Press enter to continue...')
-'''
+
 ########## Part 2: Regularized Linear Regression Cost ########## 
 tmpX = np.concatenate((np.ones((x.shape[0], 1)), x), axis=1)
 theta = np.array([[1], [1]])
 # python is a weird language, when you want to ONLY pass VALUE of an array, 
 # DO NOT pass "theta", pass "np.copy(theta)", otherwise the theta itself will
-# be changed.
+# be changed if you change theta in the function.
 cost = lr.linearRegCostFunction(tmpX, y, np.copy(theta), lamda=1)[0]
-print('Cost at theta = (1, 1) should be 303.993192\nthe calculated cost is', cost)
+print('Cost at theta = (1, 1) should be 303.993192\nthe calculated cost is', \
+        cost)
+
+input('Program paused. Press enter to continue...')
 
 ########## Part 3: Regularized Linear Regression Gradient ########## 
 grad = lr.linearRegCostFunction(tmpX, y, np.copy(theta), lamda=1)[1]
 print('Gradient at theta = (1, 1) should be (-15.303016, 598.250744)\n\
 the calculated grad is\n', grad)
 
+input('Program paused. Press enter to continue...')
+
 ########## Part 4: Train Linear Regression ########## 
-theta = lr.trainLinearReg(tmpX, y, lamda=1)
-print(theta, theta.shape)
+theta = lr.trainLinearReg(tmpX, y, lamda=0)
+plt.plot(x, x*(theta[1])+theta[0])
+plt.show()
 
 ########## Part 5: Learning Curve for Linear Regression ########## 
 tmpXv = np.concatenate((np.ones((xv.shape[0], 1)), xv), axis=1)
-lr.learningCurve(tmpX, y, tmpXv, yv, lamda=0)
+errTrain, errVal = lr.learningCurve(tmpX, y, tmpXv, yv, lamda=0)
+u, v = errTrain.shape
+tmp = np.array(range(u+1))
+plt.plot(tmp[1:], errTrain, label='Train',)
+plt.plot(tmp[1:], errVal, label='Cross Validation')
+plt.legend()
+plt.xlabel('Number of training examples')
+plt.ylabel('Error')
+plt.title('Learning curve for linear regression')
+plt.show()
+
+input('Program paused. Press enter to continue...')
 
 ########## Part 6: Feature Mapping for Polynomial Regression ########## 
-polyX = lr.polyFeatures(x, p=8)
-polyX, mean, std = lr.featureNormalize(polyX)
+# Map x onto polynomial features and normalize
+p=8
+lamda = 3
+polyX = lr.polyFeatures(np.copy(x), p)
+polyX, mean, std = lr.featureNormalize(np.copy(polyX))
+polyX = np.concatenate((np.ones((polyX.shape[0], 1)), polyX), axis=1)
 
+# Map xt onto polynomial features and normalize(using mean and std)
+polyXt = lr.polyFeatures(np.copy(xt), p)
+polyXt = polyXt - mean
+polyXt = polyXt / std
+polyXt = np.concatenate((np.ones((polyXt.shape[0], 1)), np.copy(polyXt)), axis=1)
 
-print(x)
-print(polyX)
+# Map xv onto polynomial features and normalize(using mean and std)
+polyXv = lr.polyFeatures(np.copy(xv), p)
+polyXv = polyXv - mean
+polyXv = polyXv / std
+polyXv = np.concatenate((np.ones((polyXv.shape[0], 1)), np.copy(polyXv)), axis=1)
+
+print('First Row of Normalized Training Example is', polyX[0,:])
+
+input('Program paused. Press enter to continue...')
+
+########## Part 7: Learning Curve for Polynomial Regression ########## 
+
+theta = lr.trainLinearReg(polyX, y, lamda)
+plt.plot(x, y, 'ro')
+lr.plotFit(min(x), max(x), mean, std, theta, p)
+plt.title('Polynomial Regression Fit (lambda = %s)'%lamda)
+plt.show()
+
+errTrain, errVal = lr.learningCurve(polyX, y, polyXt, yt, lamda)
+u, v = errTrain.shape
+tmp = np.array(range(u+1))
+plt.plot(tmp[1:], errTrain, label='Train',)
+plt.plot(tmp[1:], errVal, label='Cross Validation')
+plt.legend()
+plt.xlabel('Number of training examples')
+plt.ylabel('Error')
+plt.title('Polynomial Regression Learning Curve (lambda = %s)'%lamda)
+plt.show()
+
+input('Program paused. Press enter to continue...')
+
+########## Part 8: Validation for Selecting Lamda ########## 
+lamdaVec, errTrain, errVal = lr.validationCurve(polyX, y, polyXt, yt)
+plt.plot(lamdaVec[0:], errTrain, label='Train')
+plt.plot(lamdaVec[0:], errVal, label='Cross Validation')
+plt.legend()
+plt.xlabel('lambda')
+plt.ylabel('Error')
+plt.show()
+
 print(sys.version)
 
