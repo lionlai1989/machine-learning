c7d41864e58329986c6016943866c38e6fdedc8b
ChihAn Lai <chihan.lai@gmail.com>
2015-09-28 22:06:34 +0800

    finished k-means

--- ex7/kMeansClustering.py
+++ ex7/kMeansClustering.py
@@ -16,56 +16,50 @@ from plotly.graph_objs import *
 from sklearn import svm
 
 def findClosestCentroids(x, initialCentroids):
-    idx = np.zeros(0) # create (1, 2) array
+    u, v = x.shape
+    m, n = initialCentroids.shape
+    #print(u, v, m, n)
+    idx = np.zeros(0)
     for x in x:
-        a = (math.hypot(x[0]-initialCentroids[0][0], x[1]- initialCentroids[0][1]))
-        b = (math.hypot(x[0]-initialCentroids[1][0], x[1]- initialCentroids[1][1]))
-        c = (math.hypot(x[0]-initialCentroids[2][0], x[1]- initialCentroids[2][1]))
-        if a < b and a < c:
-            idx = np.append(idx, np.array([1]), axis=0)
-        elif b < c:
-            idx = np.append(idx, np.array([2]), axis=0)
-        else: 
-            idx = np.append(idx, np.array([3]), axis=0)
+        tmp = np.zeros(0)
+        for i in initialCentroids:
+            tmp = np.append(tmp, np.array([np.linalg.norm(x-i)]), axis=0)
+        idx = np.append(idx, np.array([1 + np.argmin(tmp)]), axis=0)  
     return idx
 
 def computeCentroids(x, idx, K):
-    #print(x.shape, idx.shape)
     centroids = np.zeros(shape=(K, x.shape[1]))
     for i in range(1, K+1): # i = 1 to K
-        #print(np.where(idx==i)[0])
         tmp = (np.where(idx==i)[0])
         centroids[i-1] = (np.array(np.mean(x[tmp], axis=0)))
     return centroids
 
 def plotProgressKMeans(x, centroids, previousCentroids, idx, K, i):
-
+    '''
+    I don't impliment the block of code, since I tend to leave some blank here.
+    '''
     return 1
 
 def runKMeans(x, initialCentroids, maxIters, flag):
     # initialize values
     m, n = x.shape
     K = initialCentroids.shape[0]
-    print(m, n, K)
+    #print(m, n, K)
     centroids = initialCentroids
     previousCentroids = centroids
-    
     # Run K-Means
-    for i in range(1,maxIters):
-        print('K-Means iteration', i, maxIters)
+    for i in range(1,maxIters+1):
+        print('K-Means iteration', i, 'of', maxIters)
         idx = findClosestCentroids(x, centroids)
-
         if(flag):
             plotProgressKMeans(x, centroids, previousCentroids, idx, K, i)
             previousCentroids = centroids
-
-
         centroids = computeCentroids(x, idx, K)
-    
+        print(centroids)
     return centroids, idx
 
 def kMeansInitCentroids(x, K):
-    initCentroids = np.zeros(K, x.shape[1])
-    return initCentroids
+    np.random.shuffle(x) 
+    return x[:K,:]
 
 
--- ex7/test/ex7.py
+++ ex7/test/ex7.py
@@ -4,7 +4,7 @@ import sys
 import os
 import kMeansClustering as kmc
 import scipy.io
-from scipy.optimize import fmin_cg
+from scipy.misc import imread
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,40 +15,76 @@ os.chdir(os.path.dirname(sys.argv[0]))
 ########## Part 1: Find Closest Centroids ########## 
 print('Finding closest centroids')
 data = scipy.io.loadmat("ex7data2.mat", mat_dtype=False)
-print(data)
+#print(data)
 
 x = data['X']
 print('x shape =', x.shape)
 
 K = 3
 initialCentroids = np.array([[3, 3], [6, 2], [8, 5]])
-
 idx = kmc.findClosestCentroids(x, initialCentroids)
-
 print('Closest centroids for the first 3 examples:')
 print(idx[0:3])
 print('the closest centroids should be 1, 3, 2 respectively')
+input('Program paused. Press enter to continue...')
 
 ########## Part 2: Compute Means ########## 
 print('Computing centroids means.')
 centroids = kmc.computeCentroids(x, idx, K)
+print('Centroids computed after initial finding of closest centroids:')
 print(centroids)
-########## Part 3: K-Means Clustering ########## 
+print('the centroids should be')
+print('[ 2.428301 3.157924 ]')
+print('[ 5.813503 2.633656 ]')
+print('[ 7.119387 3.616684 ]')
+input('Program paused. Press enter to continue...')
 
+########## Part 3: K-Means Clustering ########## 
 print('Running K-Means clustering on example dataset.')
 data = scipy.io.loadmat("ex7data2.mat", mat_dtype=False)
 K = 3
-maxIters = 3
-initialCentroids = np.array([[3, 3], [6, 2], [8, 5]])
-centroids, idx = kmc.runKMeans(x, initialCentroids, maxIters, True)
-print(centroids)
+maxIters = 10
+#initialCentroids = np.array([[3, 3], [6, 2], [8, 5]])
+initialCentroids = kmc.kMeansInitCentroids(x, K) 
+# if picking random centroids from example data, maxIters should be increased.
+centroids, idx = kmc.runKMeans(x, initialCentroids, maxIters, False)
+print('final centroids:\n', centroids)
+print('K-Means Done')
+input('Program paused. Press enter to continue...')
 
 ########## Part 4: K-Means Clustering on Pixels ########## 
 print('Running K-Means clustering on pixels from an image')
+pic = imread('me.png')
+
+axes = plt.gca()
+figure = plt.gcf()
+axes.imshow(pic)
+plt.show(block=True)
+
+pic = pic / 255
+a = pic.shape[0]
+b = pic.shape[1]
+c = pic.shape[2]
+# reshape pic from a*b*c to (a*b)*c
+pic = np.reshape(pic, (a*b, c))
+K = 16
+maxIters = 10
+# again, the np.copy(x) here is very important, if not, x will be changed.
+initialCentroids = kmc.kMeansInitCentroids(np.copy(pic), K)
+centroids, idx = kmc.runKMeans(np.copy(pic), initialCentroids, maxIters, False)
 
 ########## Part 5: Image Compression ########## 
 print('Applying K-Means to compress an image.')
+idx = kmc.findClosestCentroids(pic, np.copy(centroids))
+xRecovered = np.zeros(pic.shape)
+for i in range(0, idx.shape[0]):
+	tmp = int(idx[i])
+	xRecovered[i] = centroids[tmp-1]
+xRecovered = np.reshape(xRecovered, (a, b, c))
 
+axes = plt.gca()
+figure = plt.gcf()
+axes.imshow( xRecovered)
+plt.show( block=True )
 
 print(sys.version)
-
